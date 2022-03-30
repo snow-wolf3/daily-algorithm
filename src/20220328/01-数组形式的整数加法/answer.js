d5290de5ae117881047ab582403889c367ae785f
xl3_rx <xl3_rx@126.com>
2022-03-30 14:49:09 +0800

    feat: 数组形式的整数加法

---
+++ src/20220328/01-数组形式的整数加法/answer.js
@@ -0,0 +1,47 @@
+export function addToArrayForm(num, k){
+  var kArr = []
+  while (k) {
+    kArr.unshift(k % 10);
+    k = ~~(k / 10)
+  }
+  const len = num.length;
+  const kLen =  kArr.length;
+  let i = len - 1;
+  let j = kLen - 1;
+  let add = false;
+  while (i >= 0 || j >= 0) {
+    if (i >= 0) {
+      num[i] += (~~add)
+    }
+    if (j >= 0) {
+      num[i] += kArr[j];
+    }
+    if (num[i] >= 10) {
+      num[i] = num[i] % 10
+      add = true
+    } else {
+      add = false;
+    }
+    if (i === 0 && j > 0) {
+      i = 0;
+      num.unshift(0)
+    } else {
+      i--;
+    }
+    j--;
+  }
+  if (add) {
+    num.unshift(1)
+  }
+  return num
+}
+
+// var a = [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,0,0,6,3]
+var a = [7,7,5,2]
+var b = 7105
+// var a = [1]
+
+// var b = 9999
+console.log(
+  addToArrayForm(a, b)
+);
