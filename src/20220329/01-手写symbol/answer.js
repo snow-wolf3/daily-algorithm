7a029fbdc6baf720c03ff7f4a2466c83ee4ad077
xl3_rx <xl3_rx@126.com>
2022-03-31 14:59:02 +0800

    feat: 手写Symbol

--- src/20220329/01-手写symbol/answer.js
+++ src/20220329/01-手写symbol/answer.js
@@ -4,10 +4,34 @@ function MySymbol() {
     throw new Error(`${thisFnName}禁止new`);
   }
 }
-module.exports = SymbolPolyfill = function Symbol(description) {
+var create = Object.create, defineProperty = Object.defineProperty, objPrototype = Object.prototype;
+
+var created = create(null);
+function generateName(desc) {
+	var postfix = 0, name, ie11BugWorkaround;
+	while (created[desc + (postfix || "")]) ++postfix;
+	desc += postfix || "";
+	created[desc] = true;
+	name = "@@" + desc;
+	defineProperty(
+		objPrototype,
+		name,
+		d.gs(null, function (value) {
+			// For IE11 issue see:
+			// https://connect.microsoft.com/IE/feedbackdetail/view/1928508/
+			//    ie11-broken-getters-on-dom-objects
+			// https://github.com/medikoo/es6-symbol/issues/12
+			if (ie11BugWorkaround) return;
+			ie11BugWorkaround = true;
+			defineProperty(this, name, d(value));
+			ie11BugWorkaround = false;
+		})
+	);
+	return name;
+};
+function Symbol(description) {
 	var symbol;
 	if (this instanceof Symbol) throw new TypeError("Symbol is not a constructor");
-	if (isNativeSafe) return NativeSymbol(description);
 	symbol = create(HiddenSymbol.prototype);
 	description = description === undefined ? "" : String(description);
 	return defineProperties(symbol, {
